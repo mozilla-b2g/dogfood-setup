7157869e255932a76e8d8c2fd86fcbea1a94485a
Marshall Culpepper <marshall.law@gmail.com>
2012-10-02 10:19:08 -0500

    Initial commit of dogfood setup script

---
+++ README.md
@@ -0,0 +1,11 @@
+dogfood-setup
+=============
+
+A setup script for FxOS dogfood devices
+
+Usage:
+
+    $ ./dogfood-setup.sh <dogfood-id>
+
+dogfood-id should be a unique ID for tracking the dogfood device's updates and
+crash reports.
---
+++ dogfood-setup.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+# setup a FxOS device for dogfooding by assigning a unique ID
+set -e
+
+if [[ ! -n "$1" ]]; then
+  echo "Error: This script requires a unique dogfood ID to assign to the device"
+  echo "Usage: $0 <dogfood-id>"
+  exit 1
+fi
+
+DOGFOOD_ID=$1
+ADB=${ADB:-adb}
+$ADB wait-for-device
+
+# Store a property too, just in case this needs to be recovered later
+$ADB shell setprop persist.moz.dogfood.id $DOGFOOD_ID
+
+B2G_PREF_DIR=/system/b2g/defaults/pref
+TMP_DIR=/tmp/dogfood-prefs
+rm -rf $TMP_DIR
+mkdir $TMP_DIR
+
+cat >$TMP_DIR/dogfood_id.js <<DOGFOOD_ID
+pref("distribution.id", "$DOGFOOD_ID");
+pref("prerelease.dogfood.id", "$DOGFOOD_ID");
+DOGFOOD_ID
+
+cat >$TMP_DIR/dogfood_updates.js <<DOGFOOD_UPDATES
+pref("app.update.channel", "stable");
+pref("app.update.url", "http://update.boot2gecko.org/%CHANNEL%/update.xml?build_id=%BUILD_ID%&version=%VERSION%&dogfood_id=%DISTRIBUTION%");
+DOGFOOD_UPDATES
+
+$ADB remount
+$ADB push $TMP_DIR/dogfood_id.js $B2G_PREF_DIR/dogfood_id.js
+$ADB push $TMP_DIR/dogfood_updates.js $B2G_PREF_DIR/dogfood_updates.js
+
+$ADB shell "stop b2g; start b2g"
